ef2144ab739f9c46e2bd077745d75fac0dea4f45
Yahely <yahelyoseguera@hotmail.es>
2024-08-05 16:57:22 -0700

    some images added, max and min temperature are

--- src/js/script.js
+++ src/js/script.js
@@ -116,8 +116,123 @@ async function apiWeatherCall (object){
         .then((response) => response.text())
         .then((result) => {
             var result = JSON.parse(result);
+
+            const dates = result.daily.time;
+            const days = dates.map(date =>{
+                const day = new Date(date).toLocaleDateString("en-US", {weekday: "long"});
+                return day;
+            })
+
+            /* First Day */
+            let maxTemperatureFirstDay = document.getElementById("maxTemperatureFirstDay");
+            maxTemperatureFirstDay.innerHTML = result.daily.temperature_2m_max[1] + "C"; 
+            let minTemperatureFirstDay = document.getElementById("minTemperatureFirstDay");
+            minTemperatureFirstDay.innerHTML = result.daily.temperature_2m_min[1] + "C";
+            let imageFirstDay = document.getElementById("imageFirstDay");
+            imageFirstDay.src = getCodeWeather(result.daily.weather_code[1]);
+            /* IMAGE CODE HERE */
+            let firstDate = document.getElementById("firstDate")
+            firstDate.innerHTML = days[2];
+
+            /* Secound Day */
+            let maxTemperatureSecoundDay = document.getElementById("maxTemperatureSecoundDay");
+            maxTemperatureSecoundDay.innerHTML = result.daily.temperature_2m_max[2] + "C"; 
+            let minTemperatureSecoundDay = document.getElementById("minTemperatureSecoundDay");
+            minTemperatureSecoundDay.innerHTML = result.daily.temperature_2m_min[2] + "C";
+
+            let secoundtDate = document.getElementById("secoundtDate")
+            secoundtDate.innerHTML = days[3];
+        
+            /* THIRD DAY */
+            let maxTemperatureThirdDay = document.getElementById("maxTemperatureThirdDay");
+            maxTemperatureThirdDay.innerHTML = result.daily.temperature_2m_max[3] + "C"; 
+            let minTemperatureThirdDay = document.getElementById("minTemperatureThirdDay");
+            minTemperatureThirdDay.innerHTML = result.daily.temperature_2m_min[3] + "C";
+
+            let thirdDate = document.getElementById("thirdDate")
+            thirdDate.innerHTML = days[4];
+            /* FOUR DAY */
+            let maxTemperatureFourDay = document.getElementById("maxTemperatureFourDay");
+            maxTemperatureFourDay.innerHTML = result.daily.temperature_2m_max[4] + "C"; 
+            let minTemperatureFourDay = document.getElementById("minTemperatureFourDay");
+            minTemperatureFourDay.innerHTML = result.daily.temperature_2m_min[4] + "C";
+
+            let fourthDate = document.getElementById("fourthDate")
+            fourthDate.innerHTML = days[5];
+            /* FIVE  DAY  */
+            let maxTemperatureFiveDay = document.getElementById("maxTemperatureFiveDay");
+            maxTemperatureFiveDay.innerHTML = result.daily.temperature_2m_max[5] + "C"; 
+            let minTemperatureFiveDay = document.getElementById("minTemperatureFiveDay");
+            minTemperatureFiveDay.innerHTML = result.daily.temperature_2m_min[5] + "C";
+
+            let fifthDate = document.getElementById("fifthDate")
+            fifthDate.innerHTML = days[6];
+
             console.log(result);
         }) 
-        .catch((error) => console.error(error));
+        .catch((error) => console.error(error));    
 } 
 
+function getCodeWeather (value) {
+    switch(value){
+        case 0: 
+            var image = "img/sun.png";
+            break;
+        case 1:
+        case 2:
+        case 3:  
+            var image = "img/mainlyClear.png";
+            break;
+        case 45:
+        case 48:
+            var image = "img/Fog.png";
+            break;
+        case 51:
+        case 53: 
+        case 55:
+            var image = "img/Frezzing drizzle.png";
+            break;
+        case 56: 
+        case 57:
+            var image = "img/rain.png";
+            break;
+        case 61:
+        case 63: 
+        case 65:
+            var image = "img/Fog.png";
+            break;
+        case 66: 
+        case 67:
+            var image = "img/rainyshower.png";
+            break;
+        case 71:
+        case 73: 
+        case 75:
+            var image = "img/snowfall.png";
+            break;
+        case 77:
+            var image = "img/snowgrain.png";
+            break;
+        case 80:
+        case 81: 
+        case 82:
+            var image = "img/rainyshower.png";
+            break;
+        case 85: 
+        case 86:
+            var image = "img/snowshower.png";
+            break;
+        case 95:
+        case 96: 
+        case 99: 
+            var image = "img/storm.png";
+            break;
+        default:
+            var image = "img/sun.png";
+            break;
+        }
+        let finalObject = {
+            "image" : image
+        };
+        return finalObject.image;
+    }
